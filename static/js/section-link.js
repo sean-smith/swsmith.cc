ea2ac86c466ff60edd49055b6bf72b17e007f9c2
Sean Smith <seaam@amazon.com>
2024-01-31 16:52:53 -0800

    Section Header Links
    Signed-off-by: Sean Smith <seaam@amazon.com>

---
+++ static/js/section-link.js
@@ -0,0 +1,14 @@
+// this script renders all headers as clickable links
+// see https://gohugo.io/content-management/cross-references/#use-of-ref-and-relref
+window.onload = function() {
+    var allHeadings = document.getElementsByTagName("h2");
+    console.log(allHeadings);
+
+    for (var i = 0; i < allHeadings.length; i++) {
+        var heading = allHeadings[i].innerHTML;
+        var id = allHeadings[i].id;
+        allHeadings[i].innerHTML = `${heading}<a href=#${id} style='border-bottom: none;'><i data-feather="link"></i></a>`;
+        feather.replace();
+    }
+};
+
